b718fdb93a3a95cc10da665a684045ca05b2f6b2
Fillipe Soares <fillipeaguiar.soares@gmail.com>
2018-03-22 17:11:40 -0300

    Primeiro commit

---
+++ spring-batch-samples/src/main/java/br/com/fill/samples/App.java
@@ -0,0 +1,13 @@
+package br.com.fill.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
